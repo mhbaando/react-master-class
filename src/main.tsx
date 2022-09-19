7eaa67f2c42408f3dfdc9fe5f13dff8e1cb2c34c
Mohamud Abshir <maxamuudyare21@gmail.com>
2022-09-19 18:50:11 +0300

    added the styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDom from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "@/router";
-
+import "./scss/index.scss";
 const root = ReactDom.createRoot(
   document.getElementById("root") as HTMLDivElement
 );
