af00dc6120104380f35e0937f220965f9f3c4bda
Mohamud Abshir <maxamuudyare21@gmail.com>
2022-09-19 18:48:56 +0300

    added new aliasis at vitcondfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import path, { resolve } from "node:path";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,6 +8,16 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@components": path.resolve(__dirname, "./src/components"),
+      "@ui": path.resolve(__dirname, "./src/ui"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData:
+          "@import './src/scss/_helpers','./src/scss/_mixns','./src/scss/_palette','./src/scss/_variables.scss','./src/scss/_typography.scss'; ",
+      },
     },
   },
   server: {
